35c58d3f925e3fe0dbd23a22c881a599e7bcb522
mclaramarinho <marinho.claramb@gmail.com>
2026-07-19 16:58:17 -0300

    feat: nitrogen balance

---
+++ lib/shared/services/calculator/domain/use_cases/nitrogen/calculate_nitrogen_balance.usecase.dart
@@ -0,0 +1,24 @@
+// BN = (proteina ingerida (g) / 6.25) / (nitrogenio urinario 24h + 4)
+
+// Interpretação:
+// Valor negativo indica catabolismo.
+// Valor positivo indica anabolismo.
+// O objetivo em pacientes críticos é alcançar o equilíbrio (BN≈0).
+
+import 'package:nutri_calc/shared/utils/result/result.dart';
+
+class CalculateNitrogenBalance {
+  Result<double, String> call({
+    required double ingestedProtein,
+    required double urineNitrogen24h,
+  }) {
+    try {
+      if (ingestedProtein < 0 || urineNitrogen24h < 0) {
+        return Error("INVALID_PARAMS");
+      }
+      return Ok((ingestedProtein / 6.25) / (urineNitrogen24h / 4));
+    } catch (err) {
+      return Error(err.toString());
+    }
+  }
+}
