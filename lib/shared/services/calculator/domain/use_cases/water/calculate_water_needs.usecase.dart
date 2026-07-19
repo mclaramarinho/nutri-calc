870d44c6e24d67bdd8f3fc40da45dd8ceb978482
mclaramarinho <marinho.claramb@gmail.com>
2026-07-19 08:44:29 -0300

    feat: water needs

---
+++ lib/shared/services/calculator/domain/use_cases/water/calculate_water_needs.usecase.dart
@@ -0,0 +1,20 @@
+// Necessidade Hídrica:
+// Adultos: 30 - 35 ml/kg/dia (ajustar em caso de insuficiência cardíaca ou renal).
+// Idosos: 25 - 30 ml/kg/dia.
+
+import 'package:nutri_calc/shared/utils/result/result.dart';
+
+class CalculateWaterNeeds {
+  Result<double, String> call({
+    required double weight,
+    required int age,
+  }) {
+    try {
+      return Ok(
+        age >= 60 ? 25 * weight : 30 * weight
+      );
+    } catch (err) {
+      return Error(err.toString());
+    }
+  }
+}
