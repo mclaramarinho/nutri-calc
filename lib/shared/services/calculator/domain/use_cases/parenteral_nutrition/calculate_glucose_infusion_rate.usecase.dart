a21206df14e5fa29dd8c020880e70dfb0c994258
mclaramarinho <marinho.claramb@gmail.com>
2026-07-19 16:58:28 -0300

    feat: glucose infusion rate

---
+++ lib/shared/services/calculator/domain/use_cases/parenteral_nutrition/calculate_glucose_infusion_rate.usecase.dart
@@ -0,0 +1,24 @@
+// TIG = (Glicose total g x 1000) / (1400 x peso corporal kg)
+
+// Limite de segurança:
+// Em adultos, o limite de oxidação de glicose costuma ser de 4 a 5 mg/kg/min.
+// Acima disso, há risco metabólico.
+
+import 'package:nutri_calc/shared/utils/result/result.dart';
+
+class CalculateGlucoseInfusionRate {
+  Result<double, String> call({
+    required double totalGlucose,
+    required double weight,
+  }) {
+    try {
+      if (totalGlucose < 0 || weight < 0) {
+        return Error("INVALID_PARAMS");
+      }
+
+      return Ok((totalGlucose * 1000) / (1400 * weight));
+    } catch (err) {
+      return Error(err.toString());
+    }
+  }
+}
