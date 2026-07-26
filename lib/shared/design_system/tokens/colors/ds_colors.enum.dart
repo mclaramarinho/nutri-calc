0bc3b469940bce84040145f11fca73d95bf60c8f
mclaramarinho <marinho.claramb@gmail.com>
2026-07-26 18:03:20 -0300

    feat: started design system

---
+++ lib/shared/design_system/tokens/colors/ds_colors.enum.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+enum DsColors {
+  white(value: Colors.white),
+  blue(value: Colors.blue);
+
+  final Color value;
+
+  const DsColors({required this.value});
+}
